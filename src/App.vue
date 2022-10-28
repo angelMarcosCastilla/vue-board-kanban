e33efb796039896cad476bd71c56c23a0487e2d8
angelMarcosCastilla <ajmarcosc15@gmail.com>
2022-10-28 11:36:46 -0500

    init proyect

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Counter from "./components/HelloWorld.vue"
+</script>
+
+<template>
+  <h1>hola mundo</h1>
+  <Counter count="30"/>
+</template>
+
+<style scoped>
+
+</style>
